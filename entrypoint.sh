9c251d0b6c2e2809d88073a75d209b85c25fac38
凌莞 <i@gao4.pw>
2021-11-28 13:37:49 +0800

    2021年 11月 28日 星期日 13:37:49 CST

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,10 @@ yay -Sy
 
 useradd builder -m
 echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-chmod -R a+rw .
-
 git clone https://aur.archlinux.org/$pkgname.git
 cd $pkgname
 source PKGBUILD
+chmod -R a+rw .
+
 sudo --set-home -u builder yay -S --noconfirm --needed --asdeps "${makedepends[@]}" "${depends[@]}"
 sudo --set-home -u builder CARCH=aarch64 makepkg -sfA
