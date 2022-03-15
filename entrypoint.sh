c4841e99762124dccc75f745658bc8e82ce68a48
凌莞~(=^▽^=) <hujl_123@yeah.net>
2022-03-15 14:35:16 +0800

    prefer build with pentium4

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,12 +9,16 @@ echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 echo 'PACKAGER="Clansty <i@gao4.pw>"
 COMPRESSZST=(zstd -19 -c -z -q --threads=0 -)' > /home/builder/.makepkg.conf
 
-  git clone https://aur.archlinux.org/$pkgname.git
-  cd $pkgname
-  source PKGBUILD
+git clone https://aur.archlinux.org/$pkgname.git
+cd $pkgname
+source PKGBUILD
 
 chmod -R a+rw .
 
-  sudo --set-home -u builder yay -S --noconfirm --needed --asdeps --overwrite='*' "${makedepends[@]}" "${depends[@]}"
-  sudo --set-home -u builder CARCH=$ARCH makepkg -sfA --skipinteg --nodeps
+if [[ ${arch[@]/pentium4/} != ${arch[@]} ]];then
+  ARCH='pentium4'
+fi
+
+sudo --set-home -u builder yay -S --noconfirm --needed --asdeps --overwrite='*' "${makedepends[@]}" "${depends[@]}"
+sudo --set-home -u builder CARCH=$ARCH makepkg -sfA --skipinteg --nodeps
 echo ::set-output name=filelist::$(sudo --set-home -u builder CARCH=$ARCH makepkg --packagelist | xargs)
